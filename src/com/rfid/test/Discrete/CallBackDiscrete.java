187038c789f50d128fee78731c5ff00c9baeea54
rickqu <ricky_qu@hotmail.com>
2022-06-23 18:06:38 +1000

    Trying to add HTTP POST of data (currently broken)

--- src/com/rfid/test/Discrete/CallBackDiscrete.java
+++ src/com/rfid/test/Discrete/CallBackDiscrete.java
@@ -1,14 +1,68 @@
 package com.rfid.test.Discrete;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import com.rfid.callBack.CallBack.Discrete;
 
 class CallBackDiscrete implements Discrete {
 
-	public void result(String data, String antennaNo, String deviceNo,String communicationMode, String temperature) {
-		System.out.println("EPC " + data + " ���ߺ�: " + antennaNo + " �豸��: " + deviceNo + " IP�򴮿ں�: " + communicationMode);
+	private static final String API_BASE = "https://4eaff70e-88b7-482c-8205-34a365caa431.mock.pstmn.io";
+	private static final String API_TAG_DATA = "/tag_data";
+	private static final String TAG_DATA_URI = API_BASE + API_TAG_DATA;
+
+	private final URL url;
+	private final HttpURLConnection con;
+
+	public CallBackDiscrete() throws Exception {
+		try {
+			url = new URL(TAG_DATA_URI);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Connection", "keep-alive");
+			con.setRequestProperty("Keep-Alive", "timeout=5, max=100");
+			con.setRequestProperty("Accept", "application/json");
+			con.setDoOutput(true);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw e;
+		}
 	}
 
+	@Override
+	public void result(String data, String antennaNo, String deviceNo,String communicationMode, String temperature) {
+		try(OutputStream os = con.getOutputStream()) {
+    		byte[] input = data.getBytes("utf-8");
+    		os.write(input, 0, input.length);			
+		} catch (Exception e) {
+			System.out.println(e);
+			return;
+		}
+
+		try(BufferedReader br = new BufferedReader(
+ 			new InputStreamReader(con.getInputStream(), "utf-8"))) {
+    		StringBuilder response = new StringBuilder();
+    		String responseLine = null;
+    		while ((responseLine = br.readLine()) != null) {
+        		response.append(responseLine.trim());
+    		}
+    		System.out.println(response.toString());
+		} catch (Exception e) {
+			System.out.println(e);
+			return;
+		}
+	}
+
+	@Override
 	public void serialPortException(String name, boolean result) {
 		System.out.println( " ���ںţ� " + name + " ���: " + result);
+	}
+
+	public void close() {
+		con.disconnect();
 	}
 }
--- src/com/rfid/test/Discrete/Reader.java
+++ src/com/rfid/test/Discrete/Reader.java
@@ -2,6 +2,7 @@ package com.rfid.test.Discrete;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+
 import com.rfid.uhf.controller.impl.ReaderDiscrete;
 import com.rfid.uhf.service.ReaderDisService;
 import com.rfid.uhf.service.impl.ReaderDisServiceImpl;
@@ -10,6 +11,7 @@ public class Reader implements Runnable {
 
 	private static final String IPorSerialPort = "COM3"; 
 	private static final int portOrBaudRate = 9600;    
+    private static final int SLEEP_MS = 300;
 
     private final AtomicBoolean keepRunning;
 
@@ -20,18 +22,30 @@ public class Reader implements Runnable {
     @Override
     public void run() {
 		ReaderDisService service = new ReaderDisServiceImpl();
-		ReaderDiscrete reader = service.connect(IPorSerialPort, portOrBaudRate, new CallBackDiscrete());//�����豸
+        ReaderDiscrete reader;
+        CallBackDiscrete callBackDiscrete = null;
+        try {
+            callBackDiscrete = new CallBackDiscrete();
+		    reader = service.connect(IPorSerialPort, portOrBaudRate, callBackDiscrete);//�����豸
+        } catch (Exception e) {
+            System.out.println(e);
+            if (callBackDiscrete != null) {
+                callBackDiscrete.close();
+            }
+            return;
+        }
 		if(null == reader){
 			return;
 		}
 		service.beginInv(reader);	//��������
 		while (keepRunning.get()) {
             try {
-			    Thread.sleep(300);
+			    Thread.sleep(SLEEP_MS);
             } catch (Exception e) {
                 ;
             }
 		}
+        callBackDiscrete.close();
 		service.stopInv(reader); 	//ֹͣ��������
 		service.disconnect(reader); //�Ͽ�����,�ͷ��߳�
     }
